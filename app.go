40245f1417c53f58a8d12e052ce50cd62ffe104f
jade <jade@kelote.com>
2025-01-15 16:53:47 +0800

    1

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
+
+// Login checks the username and password
+func (a *App) Login(username, password string) string {
+	// 简单的登录
+	if username == "admin" && password == "password" {
+		return "Login successful!"
+	}
+	return "Invalid username or password."
+}
